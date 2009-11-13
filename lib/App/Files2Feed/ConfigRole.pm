4d0906bf811d4380ce051cb53b983dd0c1ef20bf
Pedro Melo <melo@simplicidade.org>
2009-11-13 17:15:36 +0000

    Added POD to our role for config from configuration file
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/App/Files2Feed/ConfigRole.pm
+++ lib/App/Files2Feed/ConfigRole.pm
@@ -21,3 +21,42 @@ sub get_config_from_file {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+App::Files2Feed::ConfigRole - A role for loading configuration from a file
+
+
+=head1 SYNOPSIS
+
+    # Internal module, not for direct use
+
+
+=head1 DESCRIPTION
+
+...
+
+=head1 API
+
+...
+
+=head2 get_config_from_file
+
+...
+
+
+=head1 AUTHOR
+
+Pedro Melo, C<< <melo at cpan.org> >>
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Pedro Melo.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
